b1128a8224dbcbfd909b7766ed4461d4d2686262
Nabyendu Ojha <nabyenduojha99@gmail.com>
2025-03-05 04:16:24 +0530

    added request body in get endpoint

--- src/main/java/com/ideal/controller/MainController.java
+++ src/main/java/com/ideal/controller/MainController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.*;
 public class MainController {
 
     @GetMapping("/get/{name}")
-    public ResponseEntity<String> getMathod(@PathVariable(name = "name") String userName, @RequestParam(required = false, name = "org") String organization){
-        return new ResponseEntity<>("Hi " + userName + " Good Day from " + organization, HttpStatus.OK);
+    public ResponseEntity<String> getMathod(@PathVariable(name = "name") String userName, @RequestParam(required = false, name = "org") String organization, @RequestBody(required = false) Integer countOfLaptops){
+        return new ResponseEntity<>("Hi " + userName + " Good Day from " + organization + " You have " + countOfLaptops + " Laptops", HttpStatus.OK);
     }
 
 }
