697d2179b528c470ab2412e348e12eb71187c85e
Nabyendu Ojha <nabyenduojha99@gmail.com>
2025-03-05 04:02:38 +0530

    initial commit

---
+++ src/main/java/com/ideal/SpringIdealsApplication.java
@@ -0,0 +1,13 @@
+package com.ideal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIdealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIdealsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ideal/SpringIdealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIdealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
