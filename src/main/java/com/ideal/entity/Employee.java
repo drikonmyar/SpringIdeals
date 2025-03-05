f11e31ab6ff614504b45e2bfdb48a4196f9cbdb5
Nabyendu Ojha <nabyenduojha99@gmail.com>
2025-03-06 04:27:29 +0530

    added Employee entity

---
+++ src/main/java/com/ideal/entity/Employee.java
@@ -0,0 +1,24 @@
+package com.ideal.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "employee_data")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "employee_id")
+    private Integer id;
+    @Column(name = "employee_name", nullable = false)
+    private String name;
+    @Column(name = "years_of_exp")
+    private Integer yoe;
+
+}
