afa32fa398bd2fabc02f36973f081fdbf53a76b3
Nabyendu Ojha <nabyenduojha99@gmail.com>
2025-10-20 19:53:11 +0530

    spelling correction

--- src/main/java/com/ideal/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ideal/exception/GlobalExceptionHandler.java
@@ -33,7 +33,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, Object>> handleGeneticException(Exception ex){
+    public ResponseEntity<Map<String, Object>> handleGenericException(Exception ex){
         Map<String, Object> exception  = new HashMap<>();
         exception.put("timestamp", LocalDateTime.now());
         exception.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
