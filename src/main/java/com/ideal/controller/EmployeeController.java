5a8ff54df5b6b8883ee499d3a48f499ec82372ec
Nabyendu Ojha <nabyenduojha99@gmail.com>
2025-10-22 00:05:55 +0530

    added authorizations

--- src/main/java/com/ideal/controller/EmployeeController.java
+++ src/main/java/com/ideal/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import com.ideal.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -19,26 +20,31 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @PostMapping("/create")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Employee> createEmployee(@RequestBody EmployeeDto employeeDto){
         return new ResponseEntity<>(employeeService.createEmployee(employeeDto), HttpStatus.CREATED);
     }
 
     @GetMapping("/get/{id}")
+    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<Employee> getEmployee(@PathVariable Integer id){
         return new ResponseEntity<>(employeeService.getEmployee(id), HttpStatus.OK);
     }
 
     @GetMapping("/getall")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<List<Employee>> getAllEmployees(){
         return new ResponseEntity<>(employeeService.getAllEmployees(), HttpStatus.OK);
     }
 
     @PutMapping("/update")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Employee> updateEmployee(@RequestParam Integer id, @RequestBody EmployeeDto employeeDto){
         return new ResponseEntity<>(employeeService.updateEmployee(id, employeeDto), HttpStatus.OK);
     }
 
     @PostMapping("/upload")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> uploadExcel(@RequestParam("file") MultipartFile file){
         if(file.isEmpty()){
             return new ResponseEntity<>("Please upload a valid Excel file", HttpStatus.BAD_REQUEST);
