d5404fcb14c7d114f50f1cdfb5f5f319db0824b3
Nabyendu Ojha <nabyenduojha99@gmail.com>
2026-01-11 22:02:05 +0530

    fixed high severity sonar

--- src/main/java/com/ideal/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ideal/exception/GlobalExceptionHandler.java
@@ -12,53 +12,58 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final String TIMESTAMP = "timestamp";
+    private static final String STATUS = "status";
+    private static final String ERROR = "error";
+    private static final String MESSAGE = "message";
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Map<String, Object>> handleResourceNotFoundException(ResourceNotFoundException ex){
         Map<String, Object> exception  = new HashMap<>();
-        exception.put("timestamp", LocalDateTime.now());
-        exception.put("status", HttpStatus.NOT_FOUND.value());
-        exception.put("error", HttpStatus.NOT_FOUND);
-        exception.put("message", ex.getMessage());
+        exception.put(TIMESTAMP, LocalDateTime.now());
+        exception.put(STATUS, HttpStatus.NOT_FOUND.value());
+        exception.put(ERROR, HttpStatus.NOT_FOUND);
+        exception.put(MESSAGE, ex.getMessage());
         return new ResponseEntity<>(exception, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(NoChangeException.class)
     public ResponseEntity<Map<String, Object>> handleNoChangeException(NoChangeException ex){
         Map<String, Object> exception  = new HashMap<>();
-        exception.put("timestamp", LocalDateTime.now());
-        exception.put("status", HttpStatus.BAD_REQUEST.value());
-        exception.put("error", HttpStatus.BAD_REQUEST);
-        exception.put("message", ex.getMessage());
+        exception.put(TIMESTAMP, LocalDateTime.now());
+        exception.put(STATUS, HttpStatus.BAD_REQUEST.value());
+        exception.put(ERROR, HttpStatus.BAD_REQUEST);
+        exception.put(MESSAGE, ex.getMessage());
         return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InsufficientPermissionException.class)
     public ResponseEntity<Map<String, Object>> handleInsufficientPermissionException(InsufficientPermissionException ex){
         Map<String, Object> exception  = new HashMap<>();
-        exception.put("timestamp", LocalDateTime.now());
-        exception.put("status", HttpStatus.FORBIDDEN.value());
-        exception.put("error", HttpStatus.FORBIDDEN);
-        exception.put("message", ex.getMessage());
+        exception.put(TIMESTAMP, LocalDateTime.now());
+        exception.put(STATUS, HttpStatus.FORBIDDEN.value());
+        exception.put(ERROR, HttpStatus.FORBIDDEN);
+        exception.put(MESSAGE, ex.getMessage());
         return new ResponseEntity<>(exception, HttpStatus.FORBIDDEN);
     }
 
     @ExceptionHandler(UserAlreadyExists.class)
     public ResponseEntity<Map<String, Object>> handleUserAlreadyExistsException(UserAlreadyExists ex){
         Map<String, Object> exception  = new HashMap<>();
-        exception.put("timestamp", LocalDateTime.now());
-        exception.put("status", HttpStatus.BAD_REQUEST.value());
-        exception.put("error", HttpStatus.BAD_REQUEST);
-        exception.put("message", ex.getMessage());
+        exception.put(TIMESTAMP, LocalDateTime.now());
+        exception.put(STATUS, HttpStatus.BAD_REQUEST.value());
+        exception.put(ERROR, HttpStatus.BAD_REQUEST);
+        exception.put(MESSAGE, ex.getMessage());
         return new ResponseEntity<>(exception, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, Object>> handleGenericException(Exception ex){
         Map<String, Object> exception  = new HashMap<>();
-        exception.put("timestamp", LocalDateTime.now());
-        exception.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-        exception.put("error", HttpStatus.INTERNAL_SERVER_ERROR);
-        exception.put("message", ex.getMessage());
+        exception.put(TIMESTAMP, LocalDateTime.now());
+        exception.put(STATUS, HttpStatus.INTERNAL_SERVER_ERROR.value());
+        exception.put(ERROR, HttpStatus.INTERNAL_SERVER_ERROR);
+        exception.put(MESSAGE, ex.getMessage());
         return new ResponseEntity<>(exception, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
--- src/main/java/com/ideal/security/SecurityConfig.java
+++ src/main/java/com/ideal/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.ideal.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,11 +17,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 public class SecurityConfig {
 
-    @Autowired
-    private JwtAuthFilter jwtAuthFilter;
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception{
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
